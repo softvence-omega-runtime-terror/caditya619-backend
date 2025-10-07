85a8a1c07f0c37720f768b2ef6b2cca917e4a9c5
moynul2k3 <moynul.officials@gmail.com>
2025-10-07 14:08:08 +0600

    db solved

--- app/config.py
+++ app/config.py
@@ -3,24 +3,16 @@ from pydantic_settings import BaseSettings
 from tortoise import Tortoise
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL") 
+DATABASE_URL="postgres://neondb_owner:npg_zwcn6Qk1blht@ep-dark-fire-adgbt70y-pooler.c-2.us-east-1.aws.neon.tech/neondb"
 
 class Settings(BaseSettings):
     DEBUG: bool = False
     ENV: str = "development"
-    DATABASE_URL: str
+    DATABASE_URL: str = DATABASE_URL 
     SECRET_KEY: Optional[str] = "c6dcf58058a6ce5204199818a25eed7eb58b6758a20df0385e29dbea6b49873dccad7449f8022dc193da4616ba10c97457aa2e16e0b2c5b0e5555fe1ac492aa1"
 
     class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        if not self.DEBUG:
-            env_db_url = os.environ.get("DATABASE_URL")
-            if env_db_url:
-                self.DATABASE_URL = env_db_url
+        env_file = None
                 
 settings = Settings()
 
