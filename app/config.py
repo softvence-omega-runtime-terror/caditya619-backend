7cbde0411877736aba3b473ebd33658f7a01f1fd
moynul2k3 <moynul.officials@gmail.com>
2025-10-07 10:39:29 +0600

    db solved

--- app/config.py
+++ app/config.py
@@ -2,22 +2,22 @@ from typing import Optional
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DEBUG: bool = True
+    DEBUG: bool = False
     ENV: str = "development"
-    # DATABASE_URL: str = "mysql://root:root@localhost:3306/mydb"
-    # DATABASE_URL: str = "postgres://postgres:root@localhost:5432/mydb"
-    DATABASE_URL: str = "postgres://neondb_owner:npg_kY0Qo2RUXZVL@ep-damp-scene-adfwy4vs-pooler.c-2.us-east-1.aws.neon.tech/neondb"
+    POSTGRES_URL: str = "postgres://neondb_owner:npg_kY0Qo2RUXZVL@ep-damp-scene-adfwy4vs-pooler.c-2.us-east-1.aws.neon.tech/neondb"
+    # DATABASE_URL=mysql://root:root@localhost:3306/mydb
     SECRET_KEY: Optional[str] = "c6dcf58058a6ce5204199818a25eed7eb58b6758a20df0385e29dbea6b49873dccad7449f8022dc193da4616ba10c97457aa2e16e0b2c5b0e5555fe1ac492aa1"
 
     class Config:
-        env_file = ".env"
+        env_file = ".env" 
+        env_file_encoding = "utf-8"
 
 settings = Settings()
 
-print("DATABASE", settings.DATABASE_URL)
+print("POSTGRES_URL", settings.POSTGRES_URL)
 TORTOISE_ORM = {
     "connections": {
-        "default": settings.DATABASE_URL or "sqlite://db.sqlite3"   # fallback
+        "default": settings.POSTGRES_URL or "sqlite://db.sqlite3"   # fallback
     },
     "apps": {
         "models": {
