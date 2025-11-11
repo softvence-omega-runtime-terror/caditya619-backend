2f65c47a0e66422896154af27db79fdf990f641e
Md Jahirul Islam <jahirulislam92134@gmail.com>
2025-11-11 17:29:07 +0600

    complete rider profile

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     MEDIA_DIR: str = "media/"
     MEDIA_ROOT: str = "media/"
     ENV: str = "development"
-    DATABASE_URL: str
+    DATABASE_URL: str = "sqlite://db.sqlite3"
     TWOFACTOR_API_KEY: str = "f1972b11-9a1c-11f0-b922-0200cd936042"
     SECRET_KEY: Optional[str] = None
     BASE_URL: str = "http://localhost:8000/"
