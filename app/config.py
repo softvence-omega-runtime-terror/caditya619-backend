f41f8524fab87cab53192cc4da1fe7e673921170
Moynul <moynul.officials@gmail.com>
2025-11-25 14:14:56 +0600

    pull code from nehal

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
-from decouple import config
 from typing import Optional
 from pydantic_settings import BaseSettings
 from tortoise import Tortoise
@@ -16,8 +15,8 @@ class Settings(BaseSettings):
     SECRET_KEY: Optional[str] = None
     BASE_URL: str = "http://localhost:8000/"
     RADIS_URL: str = "redis://localhost:6379/0"
-    RAZORPAY_KEY_ID: str = config("RAZORPAY_KEY_ID")
-    RAZORPAY_KEY_SECRET: str = config("RAZORPAY_KEY_SECRET")
+    RAZORPAY_KEY_ID: str
+    RAZORPAY_KEY_SECRET: str
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
