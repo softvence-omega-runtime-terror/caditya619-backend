80c1d4ea28a6d9cbb50095ed46d5e947ef90318c
Moynul <moynul.officials@gmail.com>
2025-12-08 16:49:33 +0600

    last

--- applications/user/vendor.py
+++ applications/user/vendor.py
@@ -29,8 +29,8 @@ class VendorProfile(models.Model):
     kyc_document = fields.CharField(max_length=500, null=True, blank=True)
     kyc_status = fields.CharField(max_length=20, choices=KYC_STATUS_CHOICES, default=None, blank=True, null=True)
 
-    open_time = fields.TimeField(null=True, blank=True)
-    close_time = fields.TimeField(null=True, blank=True)
+    open_time = fields.TimeField(null=True)
+    close_time = fields.TimeField(null=True)
 
     class Meta:
         table = "vendor_profile"
