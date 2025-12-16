fbdee17c24c8e36f35da287fe0eb79c60e1357ab
moynul2k3 <moynul.officials@gmail.com>
2025-12-16 10:12:10 +0600

    last

--- applications/items/models.py
+++ applications/items/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
     created_at = fields.DatetimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ["created_at"]
 
     def __str__(self):
         return self.name
