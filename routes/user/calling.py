ded3b474f3ae348a988e81c7bef2201f178179b5
moynul2k3 <moynul.officials@gmail.com>
2025-12-24 23:08:20 +0600

    last

--- routes/user/calling.py
+++ routes/user/calling.py
@@ -1,32 +1,35 @@
 import requests
 from fastapi import APIRouter, HTTPException
 from app.config import settings
-import os
-
-router = APIRouter()
+router = APIRouter(prefix='call', tags=['Call Mask'])
 
 EXOTEL_SID = settings.EXOTEL_SID
 API_KEY = settings.EXOTEL_API_KEY
 API_TOKEN = settings.EXOTEL_API_TOKEN
 CALLER_ID = settings.EXOTEL_CALLER_ID
 
-@router.post("/call/masked")
+@router.post("/masked")
 def make_masked_call(customer_number: str, agent_number: str, record: bool = True):
     url = f"https://api.exotel.com/v1/Accounts/{settings.EXOTEL_SID}/Calls/connect.json"
+
 
 
     payload = {
         "From": customer_number,
         "To": agent_number,
         "CallerId": CALLER_ID,
-        "Record": record
+        "Record": "true" if record else "false"
     }
+    print(">>>>>>>>>>>>>>>>", payload)
 
-    response = requests.post(
-        url,
-        data=payload,
-        auth=(API_KEY, API_TOKEN)
-    )
+    # response = requests.post(
+    #     url,
+    #     data=payload,
+    #     auth=(API_KEY, API_TOKEN)
+    # )
+    response = requests.post(f'https://{settings.EXOTEL_API_KEY}:{settings.EXOTEL_API_TOKEN}api.exotel.com/v1/Accounts/{settings.EXOTEL_SID}/Calls/connect', data=payload)
+
+    
 
     if response.status_code != 200:
         raise HTTPException(status_code=400, detail=response.text)
